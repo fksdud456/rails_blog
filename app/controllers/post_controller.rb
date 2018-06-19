4c21b3eed0e61a3ed516bb894b2d10a432cc94f4
fksdud456 <fksdud456@gmail.com>
2018-06-19 14:36:35 +0900

    CRUD - Create, READ

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,5 +7,21 @@ class PostController < ApplicationController
   end
 
   def create
+    # 1. 한번에 생성하기
+    Post.create(title: params[:title],
+                body: params[:body])
+
+    # 1-1. 예전버전의 문법
+    # Post.create(:title => params[:title], :body => params[:body])
+
+    # 2. 객체 만들어서 저장하기
+    # post = Post.new
+    # post.title = params[:title]
+    # post.body = params[:body]
+    # post.save
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
